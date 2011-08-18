2a20eb06a362da6f880a7b7690f4d2b8895b8b8c
Jordan R <jrjeojst@gmail.com>
2011-08-18 02:56:37 -0400

    Added first draft of R-CSP-A code

--- src/R/eeg_training_functions.R
+++ src/R/eeg_training_functions.R
@@ -1,21 +1,19 @@
 library(fastICA)
 
-nn_size <- 3
-nn_decay <- 5e-4
-nn_maxit <- 600
-nn_rang <- 0.1
-
 min_certainty <- 0.65
 
-fs <- 250
-
-test.cl <- function(true, pred) {
-	true <- max.col(true)
-	cres <- max.col(pred)
-	table(true, cres, useNA = "always")
-}
+fs <- 200
 
 # Supplementary functions
+make_eigenvalues_positive <- function (eigenvalue_object){
+	values_to_change <- seq_along(eigenvalue_object$values)[eigenvalue_object$values < 0]
+	for (i in values_to_change){
+		eigenvalue_object$vectors[,i] <- -1*eigenvalue_object$vectors[,i]
+		eigenvalue_object$values[i] <- -1*eigenvalue_object$values[i]
+	}
+	return (list(values = eigenvalue_object$values, vectors = eigenvalue_object$vectors))
+}
+
 pst <- function (df) {
 	len <- length(df)
 	ps <- rep(0, len / 2)
@@ -49,12 +47,88 @@ make_decision <- function (prob_vector){
 	return (max_index)
 }
 
+mean_rows <- function (M){
+	numSamps <- dim(M)[1]
+	m <- dim(M)[2]
+	for (i in 1:numSamps) m <- m + M[i,]
+	return (m / numSamps)
+}
+
+get_var_mat <- function (S, mean){
+	numVals <- length(mean)
+	S2 <- matrix(rep(0, numVals*numVals), numVals, numVals)
+	for (i in 1:dim(S)[1]) S2 <- S2 + (S[i,] - mean) %*% t(S[i,] - mean)
+	return (S2)
+}
+
+edist <- function (v1, v2){
+	dist <- 0
+	for (i in 1:length(v1)) dist <- dist + (v1[i] - v2[i])^2
+	return (dist^(1/2))
+}
+
+get_nnc_class <- function (testVector, featureVectors){
+	dist <- edist(testVector, featureVectors[1])
+	max_dist <- dist
+	min_dist <- dist
+	for (i in 1:length(featureVectors)){
+		dist <- edist(testVector, featureVectors[i])
+		if (dist > max_dist) max_dist <- dist
+		if (dist < min_dist) min_dist <- dist
+	}
+	return(list(DMAX = max_dist, DMIN = min_dist))
+}
+
+get_nnc_rcspa <- function (testVector, featureVectors, featureVectorLabels){
+	numFV <- dim(featureVectors)[1]
+	ind_classone <- seq_along(featureVectorLabels)[featureVectorLabels == "1"]
+	C1_metrics <- get_nnc_class(testVector, featureVectors[ind_classone,])
+	C2_metrics <- get_nnc_class(testVector, featureVectors[-ind_classone,])
+	DMAX <- max(C1_metrics$DMAX, C2_metrics$DMAX)
+	DMIN <- min(C1_metrics$DMIN, C2_metrics$DMIN)
+	DE_C1 <- (C1_metrics$DMIN - DMIN) / (DMAX - DMIN)
+	DE_C2 <- (C2_metrics$DMIN - DMIN) / (DMAX - DMIN)
+	return (list(D_C1 = DE_C1, D_C2 = DE_C2))
+}
+
+get_nnc_rcspa_mat <- function (testMat, featureVectors, featureVectorLabels){
+	numTests <- dim(testMat)[1]
+	scores <- matrix(rep(0, 2*numTests),numTests,2)
+	for (i in 1:numTests) {
+		x <- get_nnc_rcspa(testMat[i,], featureVectors, featureVectorLabels)
+		scores[i,1] <- x$D_C1
+		scores[i,2] <- x$D_C2
+	}
+	return (scores)
+}
+
 # Pre-processing functions
+fda_components <- function (data_mat, dataTrials){
+	ind_classone <- seq_along(dataTrials)[dataTrials == "1"]
+	ind_classtwo <- (1:length(dataTrials))[-ind_classone]
+	
+	S1_samples <- data_mat[ind_classone,]
+	S2_samples <- data_mat[ind_classtwo,]
+
+	S1_mean <- mean_rows(S1_samples)
+	S2_mean <- mean_rows(S2_samples)
+
+	S1_var <- get_var_mat(S1_samples, S1_mean)
+	S2_var <- get_var_mat(S2_samples, S2_mean)
+
+	SW <- S1_var + S2_var
+	SB <- (S1_mean - S2_mean) %*% t(S1_mean - S2_mean)
+
+	#return(list(S1VAR=S1_var, S2VAR=S2_var, SW=SW, SB=SB))
+
+	v <- solve(SW) %*% (S1_mean - S2_mean)
+	return(v)
+}
 
 ica_components <- function (trainingData, number_components){
 	scaling <- scale(trainingData, scale = FALSE)
-	trainingData <- trainingData - scaling
-	ica <- fastICA(trainingData, number_components, alg.typ = "parallel", fun = "logcosh", alpha = 1, method = "R", row.norm = FALSE, maxit = 200, tol = 0.0001)
+	#trainingData <- trainingData - scaling
+	ica <- fastICA(trainingData, number_components, alg.typ = "parallel", fun = "logcosh", alpha = 1, method = "C", row.norm = FALSE, maxit = 200, tol = 0.0001)
 	return (list(centering = scaling, whitening = ica$K, unmixing = ica$W, mixing = ica$A))
 }
 
@@ -63,22 +137,44 @@ pca_components <- function (trainingData, number_components){
 	return (list(centering = icaComp$centering, whitening = icaComp$whitening))
 }
 
-csp_components <- function (trainingTrials, trainingTrialClasses, trainingTrialSubjects, numTrialClassOne, numTrialClassTwo, gamma, beta){
+# single-subject
+csp_components <- function (trainingTrials, trainingTrialClasses, gamma, alpha){
+	numchannels <- dim(trainingTrials[[1]])[1]
 	trainingTrialLen <- length(trainingTrials)
-	trainingTrialVars <- trainingTrialLen	
+	trainingTrialVars <- trainingTrials
 	for (i in 1:trainingTrialLen) {
 		curMat <- trainingTrials[[i]]
 		curMat <- curMat %*% t(curMat)
 		trainingTrialVars[[i]] <- curMat/matrix.trace(curMat)
 	}
-	
+	ind_classone <- seq_along(trainingTrialClasses)[trainingTrialClasses == 1]
+	ind_classtwo <- (1:trainingTrialLen)[-ind_classone]
+	sum_classone <- trainingTrialVars[[ind_classone[1]]]
+	for (i in ind_classone[-1]) sum_classone <- sum_classone + trainingTrialVars[[i]]
+	sum_classtwo <- trainingTrialVars[[ind_classtwo[1]]]
+	for (i in ind_classtwo[-1]) sum_classtwo <- sum_classtwo + trainingTrialVars[[i]]
+	sum_classone <- sum_classone / length(ind_classone)
+	sum_classtwo <- sum_classtwo / length(ind_classtwo)
+	sum_classone <- (1 - gamma)*sum_classone + (gamma / numchannels)*diag(numchannels)
+	sum_classtwo <- (1 - gamma)*sum_classtwo + (gamma / numchannels)*diag(numchannels)
+	sigma <- sum_classone + sum_classtwo
+	eig <- make_eigenvalues_positive(eigen(sigma))
+	Lamda <- diag(eig$values^(-1/2))
+	U <- eig$vectors
+	B <- make_eigenvalues_positive(eigen(Lamda %*% t(U) %*% sum_classone %*% U %*% Lamda))$vectors
+	#return (list(B = B, Lamda = Lamda, U = U, Sigma = sigma, Sigma_1 = sum_classone))
+	W_0 <- t(B) %*% Lamda %*% t(U)
+	W <- W_0 [, c(1:alpha, (numchannels - (alpha - 1)):numchannels)]
+	return (W)
 }
 
 # Feature extraction functions
 
 pca_fe <- function (testingData, pcaComp){
-	testingData <- testingData - pcaComp$centering
+	testingData <- as.numeric(as.matrix(testingData))
+	testingData <- testingData - pcaComp$centering [1,]
 	testingData <- testingData %*% pcaComp$whitening
+	#testingData <- pcaComp$whitening %*% testingData
 	return (testingData)
 }
 
@@ -93,10 +189,10 @@ sfa_fe <- function (data) {
 	fi <- 0.5 / (fs / length(data_dft))
 	delta <- max(data_dft[fi:(8*fi)])
 	theta <- max(data_dft[(8*fi + 1):(16*fi)])
-	alpha <- max(data_dft[(16*fi + 1):(28*fi)]
-	beta <- max(data_dft[(28*fi + 1):(60*fi)]
+	alpha <- max(data_dft[(16*fi + 1):(28*fi)])
+	beta <- max(data_dft[(28*fi + 1):(60*fi)])
 	gamma <- max(data_dft[(60*fi):(length(data_dft))])
-	return (c(delta, theta, alpha, beta, gamma) / (delta + theta + alpha + beta + gamma)
+	return (c(delta, theta, alpha, beta, gamma) / (delta + theta + alpha + beta + gamma))
 }
 
 psa_fe <- function (data) {
@@ -119,6 +215,36 @@ stacking_fe <- function (classifier_probs) {
 	return (fv)
 }
 
-rcspa_fe <- function (testingTrial, cspComp){
+#return (list(Sigma_1 = sum_classone, Sigma_2 = sum_classtwo, Lambda = diag(eig$values), U = eig$vectors))
 
+# MUST HAVE LAMDA BE ALL POSITIVE!
+rcspa_fe <- function (testingTrial, W){
+	X <- t(W) %*% testingTrial
+	Q <- dim(W)[2]
+	XVar <- 1:Q
+	for (i in 1:Q) XVar[i] <- var(X[i,])
+	XVarSum <- sum(XVar)
+	Y <- 1:Q
+	for (i in 1:length(Y)) Y[i] <- log(XVar[i] / XVarSum)
+	return (Y)
+}
+
+rcspa_fe_mat <- function (testingTrials, W){
+	numFeatures <- length(testingTrials)
+	Q <- dim(W)[2]
+	features <- matrix(rep(0, numFeatures*Q), numFeatures,Q)
+	for (i in 1:length(testingTrials)) features[i,] <- rcspa_fe(testingTrials[[i]], W)
+	return (features)
+}
+
+fda_fe <- function (testingTrial, V){
+	return (t(V) %*% t(testingTrial))
+}
+
+fda_fe_mat <- function(testingTrials, V){
+	numTrials <- dim(testingTrials)[1]
+	numFeats <- 1#dim(testingTrials)[2]
+	features <- matrix(rep(0, numTrials*numFeats), numTrials, numFeats)
+	for (i in 1:numTrials) features[i,] <- fda_fe(testingTrials, V)
+	return(features)
 }
---
+++ src/R/training.R
@@ -0,0 +1,120 @@
+library(nnet)
+source("~/WinlabEEG/src/R/eeg_training_functions.R")
+
+nn_size <- 10
+nn_decay <- 5e-4
+nn_maxit <- 900
+nn_rang <- 0.1
+
+alpha <- 5
+
+assemble_trial <- function (tdata, c1_data, c2_data, curTrial, samp){
+	len <- length(tdata)
+	for (i in 1:(len / 2)){
+		tdata[[i]] <- as.matrix(t(c1_data[(curTrial - 1)*1000 + (i - 1)*500 + samp, 3:16]))
+		tdata[[(i + len / 2)]] <- as.matrix(t(c2_data[(curTrial - 1)*1000 + (i - 1)*500 + samp, 3:16]))
+	}
+	return (tdata)
+}
+
+combine_list <- function (newList, list1, list2, list3){
+	len1 <- length(list1)
+	len2 <- length(list2)
+	len3 <- length(list3)
+	for (i in 1:len1) newList[[i]] <- list1[[i]]
+	for (i in 1:len2) newList[[i + len1]] <- list2[[i]]
+	for (i in 1:len3) newList[[i + len1 + len2]] <- list3[[i]]
+	return(newList)
+}
+
+run_rcspa <- function (c1_data, c2_data){
+	siter <- sample(0:9,1)
+	samps <- seq(from = 1, to = 499, by = 20) + siter
+	samps2 <- seq(from = 1, to = 499, by = 20) + sample((0:9)[-(siter + 1)],1)
+	testTrial <- sample(1:3,1)
+	trainTrials <- c(1, 2, 3)[-testTrial]
+
+	list4 <- list(A=1, B=1, C=1, D=1)
+	trainData <- list(A=1, B=1, C=1, D=1, E=1, F=1, G=1, H=1, I=1, J=1, K=1,L=1)
+	t1 <- assemble_trial(list4, c1_data, c2_data, 1, samps)
+	t2 <- assemble_trial(list4, c1_data, c2_data, 2, samps)
+	t3 <- assemble_trial(list4, c1_data, c2_data, 3, samps)
+	trainData <- combine_list(trainData, t1, t2, t3)
+
+	testData <- list(A=1, B=1, C=1, D=1, E=1, F=1, G=1, H=1, I=1, J=1, K=1, L=1)
+	t1 <- assemble_trial(list4, c1_data, c2_data, 1, samps2)
+	t2 <- assemble_trial(list4, c1_data, c2_data, 2, samps2)
+	t3 <- assemble_trial(list4, c1_data, c2_data, 3, samps2)
+	testData <- combine_list(testData, t1, t2, t3)
+
+	classLabels <- rep(c(rep("1",2), rep("2",2)), 3)
+	
+	G <- seq(from = 0, to = 0.9, by = 0.2)
+	Ws <- list(A=1, B=1, C=1, E=1, F=1)
+	FDA_Vs <- list(A=1, B=1, C=1, E=1, F=1)
+	Scores_C1 <- matrix(rep(0, 5*12), 12, 5)
+	Scores_C2 <- Scores_C1
+	
+	#return(list(TRAIN=trainData, TEST=testData, LABELS=classLabels))	
+
+	for (i in 1:length(G)){
+		gamma <- G[i]
+		Ws[[i]] <- csp_components(trainData, classLabels, gamma, alpha)
+		curTrainX <- rcspa_fe_mat(trainData, Ws[[i]])		
+		#return(list(curTrainX = curTrainX, W = W, LABELS=classLabels))	
+		FDA_Vs[[i]] <- fda_components(curTrainX, classLabels)
+		curTrainZ <- curTrainX %*% FDA_Vs[[i]]#fda_fe_mat(curTrainX, FDA_Vs[[i]])
+		curTestX <- rcspa_fe_mat(testData, Ws[[i]])
+		curTestZ <- curTestX %*% FDA_Vs[[i]]#fda_fe_mat(curTestX, FDA_Vs[[i]])
+		getDE <- get_nnc_rcspa_mat(curTestZ, curTrainZ, classLabels)
+		for (j in 1:12){
+			Scores_C1[j,i] <- getDE[j,1]
+			Scores_C2[j,i] <- getDE[j,2]
+		}
+	}
+	OVERALL_SCORE <- rep(0, 12)
+	for (i in 1:12){
+		if (sum(Scores_C1[i,]) > sum(Scores_C2[i,])) OVERALL_SCORE[i] <- "2"
+		if (sum(Scores_C1[i,]) < sum(Scores_C2[i,])) OVERALL_SCORE[i] <- "1"
+		if (sum(Scores_C1[i,]) == sum(Scores_C2[i,])) OVERALL_SCORE[i] <- "0"
+	}
+	
+	return (OVERALL_SCORE)
+}
+
+
+# Actions
+# larm, lclench, lpivot, pull, push, rarm, rclench, rpivot
+data <- read.csv(file="~/WinlabEEG/training_data/trainData.csv",head=TRUE,sep=",")
+
+larm_data <- data[(data$Action == "larm"),]
+rarm_data <- data[(data$Action == "rarm"),]
+
+samp <- seq(from = 1, to = 500, by = 10)
+
+training_data <- list(A = as.matrix(t(larm_data[samp,3:16])), B = as.matrix(t(larm_data[samp + 500, 3:16])), C = as.matrix(t(larm_data[samp + 1000, 3:16])), D = as.matrix(t(larm_data[samp + 1500, 3:16])), E = as.matrix(t(larm_data[samp + 2000, 3:16])), F = as.matrix(t(larm_data[samp + 2500, 3:16])), I = as.matrix(t(rarm_data[samp,3:16])), J = as.matrix(t(rarm_data[samp + 500, 3:16])), K = as.matrix(t(rarm_data[samp + 1000, 3:16])), L = as.matrix(t(rarm_data[samp + 1500, 3:16])), M = as.matrix(t(rarm_data[samp + 2000, 3:16])), N = as.matrix(t(rarm_data[samp + 2500, 3:16])), O = as.matrix(t(larm_data[samp + 1, 3:16])), P = as.matrix(t(rarm_data[samp + 1, 3:16])))
+
+T <- run_rcspa(larm_data, rarm_data)
+
+#test.cl <- function(true, pred) {
+#	true <- max.col(true)
+#	cres <- max.col(pred)
+#	table(true, cres, useNA = "always")
+#}
+#training_data <- rbind(larm_data[1:2000, 3:16], rarm_data[1:2000, 3:16])
+#trainingTargets <- class.ind (c(rep("larm", 2000), rep("rarm", 2000)))
+#testing_data <- rbind(larm_data[2001:3000, 3:16], rarm_data[2001:3000, 3:16])
+#testingTargets <- class.ind (c(rep("larm", 1000), rep("rarm", 1000)))
+#trainingSamps <- sample (1:4000, 4000, replace = FALSE)
+#testingSamps <- sample (1:2000, 2000, replace = FALSE)
+#training_data <- training_data[trainingSamps,]
+#trainingTargets <- trainingTargets[trainingSamps,]
+#testing_data <- testing_data[testingSamps,]
+#testingTargets <- testingTargets[testingSamps,]
+#pcaComps <- pca_components(training_data, dim(training_data)[2])
+#trainingFeatures <- training_data
+#for (i in 1:2000) trainingFeatures[i,] <- pca_fe(training_data[i,], pcaComps)
+#testingFeatures <- testing_data
+#for (i in 1:1000) testingFeatures[i,] <- pca_fe(testing_data[i,], pcaComps)
+#nn <- nnet(x = trainingFeatures, y = trainingTargets, size = nn_size, rang = nn_rang, decay = nn_decay, maxit = nn_maxit)
+#result <- test.cl (testingTargets, predict(nn, testingFeatures))
