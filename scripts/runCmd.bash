0c474427cf0958cb7901c4b2b444f0f5fbeb96c3
William Pan <panman0180@gmail.com>
2011-08-19 16:04:10 -0300

    Edited scripts/runCmd.bash via GitHub

--- scripts/runCmd.bash
+++ scripts/runCmd.bash
@@ -11,30 +11,17 @@ export ROS_IP=localhost
 
 case $1 in
 	"1")
-		echo "rosservice call /tank true 200 200";;
-<<<<<<< HEAD
-		#rosservice call /tank true 200 200;;
-	"2")
-		echo "rosservice call /brake true";;
-		#rosservice call /brake true;;
-	"3")
-		echo "rosservice call /turn true 200";;
-		#rosservice call /turn true 200;;
-	"4")
-		echo "rosservice call /turn -- true -200";;
-		#rosservice call /turn -- true -200;;
-=======
+		echo "rosservice call /tank true 200 200"
 		rosservice call /tank true 200 200;;
 	"2")
-		echo "rosservice call /brake true";;
+		echo "rosservice call /brake true"
 		rosservice call /brake true;;
 	"3")
-		echo "rosservice call /turn true 200";;
+		echo "rosservice call /turn true 200"
 		rosservice call /turn true 200;;
 	"4")
-		echo "rosservice call /turn -- true -200";;
+		echo "rosservice call /turn -- true -200"
 		rosservice call /turn -- true -200;;
->>>>>>> 8752b43b1ba9af2d6b2bd85b3947795a6e4e633f
 	*)
 		echo "[ERROR] Invalid Argument"
 		exit 0;
